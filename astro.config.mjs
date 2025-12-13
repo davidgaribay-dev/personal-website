cd704116e187968c7784404e69d7fdc13f9be5d6
David Garibay <me@davidgaribay.dev>
2025-12-13 12:04:49 -0500

    fix: implement better SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://davidgaribay.dev',
   output: 'server',
   adapter: vercel(),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
